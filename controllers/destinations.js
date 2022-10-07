a7a375f11d4c4296026d39829577bd2c5dc66cf4
nukaterrapin7 <pythonism007@gmail.com>
2022-10-07 16:06:33 -0500

    Fixes Add Destination

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,12 +1,11 @@
 const Flight = require('../models/flight');
-const flights = require('./flights');
 
 module.exports = {
     create
 };
 
 function create(req, res) {
-    flights.findById(req.params.id, function(err, flight) {
+    Flight.findById(req.params.id, function(err, flight) {
         flight.destinations.push(req.body)
         flight.save(function(err) {
             res.redirect(`/flights/${flight._id}`)
